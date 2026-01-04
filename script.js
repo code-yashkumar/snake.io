c775b2ba0e1cd932d7bf225cd53e0950cf36a156
code-yashkumar <code.yashkumar@gmail.com>
2026-01-04 21:49:50 +0530

    Snake Movement behaviour complete

--- script.js
+++ script.js
@@ -1,12 +1,83 @@
 const board = document.querySelector('.board');
-const blockHeight=30;
-const blockWidth=30;
+const blockHeight=50;
+const blockWidth=50;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+let intervalId=null;
 
-for(let i=0;i<rows*cols;i++){
-    const block = document.createElement('div');
-    block.classList.add('block');
-    board.appendChild(block);
+const blocks = {};
+const snake=[
+    {x:1,y:3},
+    // {x:1,y:4},
+    // {x:1,y:5},
+]
+
+let direction='down';
+
+for(let row=0;row<rows;row++){
+    for(let col=0;col<cols;col++){
+        const block = document.createElement('div');
+        block.classList.add('block');
+        board.appendChild(block);
+        // block.innerText=`${row},${col}`;
+        blocks[`${row},${col}`]=block;
+    }
 }
+
+function renderSnake(){
+    snake.forEach(segment=>{
+        // console.dir(segment);
+        // console.log(blocks[`${segment.x},${segment.y}`]);
+        const block=blocks[`${segment.x},${segment.y}`];
+        block.classList.add('fill');
+    })
+};
+
+intervalId=setInterval(()=>{
+    let head=null;
+    if(direction==='down'){
+        head={x:snake[0].x+1,y:snake[0].y};
+    }
+    else if(direction==='up'){
+        head={x:snake[0].x-1,y:snake[0].y};
+    }
+    else if(direction==='left'){
+        head={x:snake[0].x,y:snake[0].y-1};
+    }
+    else if(direction==='right'){
+        head={x:snake[0].x,y:snake[0].y+1};
+    }
+
+    if(head.x<0 || head.x>=rows || head.y<0 || head.y>=cols){
+        alert('Game Over');
+        clearInterval(intervalId);
+        return;
+    }
+
+    snake.forEach(segment=>{
+        const block=blocks[`${segment.x},${segment.y}`];
+        block.classList.remove('fill');
+    });
+    snake.unshift(head);
+    snake.pop();
+
+
+    renderSnake();
+},400);
+
+addEventListener('keydown',(e)=>{
+    // console.log(e.key);
+    if((e.key==='ArrowDown' || e.key==='s' || e.key==='S') && direction!=='up'){
+        direction='down';
+    }
+    else if((e.key==='ArrowUp' || e.key==='w' || e.key==='W') && direction!=='down'){
+        direction='up';
+    }
+    else if((e.key==='ArrowLeft' || e.key==='a' || e.key==='A') && direction!=='right'){
+        direction='left';
+    }
+    else if((e.key==='ArrowRight' || e.key==='d' || e.key==='D') && direction!=='left'){
+        direction='right';
+    }
+});
